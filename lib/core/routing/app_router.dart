f4f61f9463efc3f08432110c0462bc165ba983c4
reemy <reem66onez@gmail.com>
2024-12-07 12:23:55 +0300

    create onBoarding and login screens and add their routes to AppRouter file

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:reservations_system/core/routing/routes.dart';
+import 'package:reservations_system/features/login/presentation/login_screen.dart';
+import 'package:reservations_system/features/on_boarding/presentation/on_boarding_screen.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.onBoardingScreen:
-        return MaterialPageRoute(builder: (_) => const Placeholder());
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => const Placeholder());
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => const Scaffold(
