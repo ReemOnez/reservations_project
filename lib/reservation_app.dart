8d110927825994d0715e21ba479dc0c2ce73d99b
reemy <reem66onez@gmail.com>
2024-12-07 12:15:59 +0300

    clean the main file and call the initial widget "ReservationApp" inside the runApp function

---
+++ lib/reservation_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReservationApp extends StatelessWidget {
+  const ReservationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
