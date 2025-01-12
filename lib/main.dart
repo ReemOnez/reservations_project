dc09d5a079f5ea239adeb9acd5caf7ed06e9e646
reemy <reem66onez@gmail.com>
2025-01-12 11:36:20 +0300

    Integrate with firebase, add firebase core package set up all platforms (android, ios, web)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reservations_system/core/flavors/flavors_cconfig.dart';
 import 'package:reservations_system/core/routing/app_router.dart';
+import 'package:reservations_system/firebase_options.dart';
 import 'package:reservations_system/reservation_app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   initApp();
   debugPrint(FlavorConfig.isProduction() ? 'prod flavor' : 'dev flavor');
   runApp(ReservationApp(appRouter: AppRouter()));
@@ -11,9 +14,13 @@ Future<void> main() async {
 
 Future<void> initApp() async {
   FlavorConfig(
-    flavor: FlavorConfig.stringToEnum(const String.fromEnvironment('flavor', defaultValue: 'dev')),
+    flavor: FlavorConfig.stringToEnum(const String.fromEnvironment('flavor', defaultValue: 'prod')),
     values: FlavorValues(
       stagesURLs: {Flavor.dev: 'https://api.bookit.com/api/', Flavor.prod: 'https://api.bookit.com/api/'},
     ),
+  );
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 }
